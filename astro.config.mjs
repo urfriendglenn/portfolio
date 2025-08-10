571836f95a44274d8ce14fb59a06de63d03b97df
glewis <glewis@digitalocean.com>
2025-08-09 21:56:02 -0500

    Added github config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://urfriendglenn.github.io',
 	integrations: [mdx(), sitemap()],
+	base: '/portfolio/'
 });
