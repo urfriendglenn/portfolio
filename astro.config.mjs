889802285e8492bda62728c5b22a4ab5ded90de9
glewis <glewis@digitalocean.com>
2025-11-03 20:40:07 -0600

    Added mermaid support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import mermaid from 'astro-mermaid';
+
 const dev = process.env.NODE_ENV === 'development';
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://glennlewis.me",
-	integrations: [mdx(), sitemap()],
-	base: dev ? '/' : '/portfolio/',
+    site: "https://glennlewis.me",
+    integrations: [mdx(), sitemap(), mermaid()],
+    base: dev ? '/' : '/portfolio/',
 });
